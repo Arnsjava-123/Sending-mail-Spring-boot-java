544c8df75c3e9cb820a55067c8a49ea5d0ad0bd7
natirajagithub <natirajaprajapati5@gmail.com>
2024-03-21 12:28:31 +0530

    mail commit

---
+++ src/main/java/com/example/mailsend/controller/EmailSendController.java
@@ -0,0 +1,33 @@
+package com.example.mailsend.controller;
+
+import com.example.mailsend.service.EmailService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSendController {
+    private EmailService emailService;
+
+    public EmailSendController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+    
+    
+    @GetMapping("/test")
+    public String test() {
+    	return "Testing API...........";
+    }
+
+    @PostMapping("/send")
+    public String sendMail(@RequestParam(value = "file", required = false) MultipartFile[] file, String to, String[] cc, String subject, String body) {
+        return emailService.sendMail(file, to, cc, subject, body);
+    }
+
+}
